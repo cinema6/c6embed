9e263af531dfae1bdd3aadbae76523b78704624e
Joshua Minzner <josh@minzner.org>
2015-11-02 12:54:47 -0500

    Refactor c6mraid to use the player service.

--- src/c6mraid/c6mraid.js
+++ src/c6mraid/c6mraid.js
@@ -1,50 +1,24 @@
-var q = require('q');
+/* jshint strict:false */
 var MRAID = require('../../lib/iab').MRAID;
-var importScripts = require('../../lib/importScripts');
-var app = require('../app/app');
-var googleAnalytics = require('../../lib/google_analytics');
-var pagePathFor = require('../ga_helpers').pagePath;
+var Player = require('../../lib/Player');
 var formatUrl = require('url').format;
-var parseUrl = require('url').parse;
+var resolveUrl = require('url').resolve;
+var extend = require('../../lib/fns').extend;
 var globalLogger = require('../../lib/logger').default;
 var logger = globalLogger.context('c6mraid.js');
-
-var modernizr = require('Modernizr');
-var C6Query = require('../../lib/C6Query');
-var Config = require('../app/Config');
-var asEvented = require('asEvented');
-var UserAgent = require('../../lib/UserAgent');
-var PostMessage = require('../../lib/PostMessage');
-var Location = require('../app/utils/Location');
-var BrowserInfo = require('../../lib/BrowserInfo');
-var PlayerProvider = require('../app/PlayerProvider');
-var C6AJAX = require('../../lib/c6ajax/C6AJAX');
-var AdLib = require('../../src/app/AdLib');
-var SponsoredCards = require('../app/SponsoredCards');
-var DocumentParser = require('../app/DocumentParser');
-var FrameFactory = require('../app/FrameFactory');
-var HostDocument = require('../app/HostDocument');
-var ObservableProvider = require('../../lib/ObservableProvider');
 
 var PLAYER_EVENTS = ['launch', 'adStart', 'adCount', 'adEnded'];
 
 logger.tasks.send.push(sendLog);
 
-function omit(object, keys) {
-    'use strict';
-
-    return Object.keys(object).reduce(function(result, key) {
-        if (keys.indexOf(key) < 0) {
-            result[key] = object[key];
-        }
-
+function pick(object, keys) {
+    return keys.reduce(function(result, key) {
+        result[key] = object[key];
         return result;
     }, {});
 }
 
 function sendLog(logger, method, args) {
-    'use strict';
-
     var img = new Image();
     img.src = formatUrl({
         protocol: 'https:',
@@ -63,153 +37,7 @@ function sendLog(logger, method, args) {
     });
 }
 
-function getLoader(apiRoot) {
-    'use strict';
-
-    window.c6 = {
-        embeds: [],
-        gaAcctIdPlayer: (function(acc,mi,mx){
-            return acc+'-'+parseInt(((Math.random()*999999999)%(mx-mi+1))+mi,10);
-        }('UA-44457821',31,35)),
-        gaAcctIdEmbed: (function(acc,mi,mx){
-            return acc+'-'+parseInt(((Math.random()*999999999)%(mx-mi+1))+mi,10);
-        }('UA-44457821',6,30))
-    };
-
-    window.__C6_URL_ROOT__ = apiRoot;
-
-    googleAnalytics('__c6_ga__', 'c6', window.c6.gaAcctIdPlayer, {
-        storage: 'none',
-        cookieDomain: 'none'
-    });
-
-    /* Create Location */
-    var $location = new Location({
-        window: window,
-        document: document
-    });
-
-    /* Create C6Query */
-    var c6Query = new C6Query({
-        document: document,
-        window: window
-    });
-
-    /* Create Config */
-    var config = new Config({
-        window: window,
-        document: window.document,
-        location: $location
-    });
-
-    /* Create UserAgent */
-    var userAgent = new UserAgent({
-        window: window
-    });
-
-    /* Create PostMessage */
-    var postmessage = new PostMessage({
-        asEvented: asEvented,
-        q: q,
-        window: window
-    });
-
-    /* Create BrowserInfo */
-    var browserInfo = new BrowserInfo({
-        modernizr: modernizr,
-        window: window,
-        userAgent: userAgent,
-        $: c6Query
-    });
-
-    /* Create Player */
-    var Player = new PlayerProvider({
-        postmessage: postmessage,
-        q: q
-    });
-
-    /* Create C6AJAX */
-    var c6Ajax = new C6AJAX({
-        window: window,
-        q: q,
-        browserInfo: browserInfo,
-        location: $location
-    });
-
-    /* Create AdLib */
-    var adLib = new AdLib({
-        c6Ajax: c6Ajax,
-        location: $location,
-        q: q
-    });
-
-    /* Create SponsoredCards */
-    var spCards = new SponsoredCards({
-        window: window,
-        config: config,
-        q: q,
-        adLib: adLib,
-        importScripts: importScripts
-    });
-
-    /* Create and Configure DocumentParser */
-    var documentParser = new DocumentParser();
-
-    /* Create and Configure FrameFactory */
-    var frameFactory = new FrameFactory({
-        $: c6Query,
-        documentParser: documentParser,
-        q: q
-    });
-
-    /* Create and Configure HostDocument */
-    var hostDocument = new HostDocument({
-        $: c6Query,
-        window: window
-    });
-
-    /* Create and Configure Observable */
-    var Observable = new ObservableProvider();
-
-    /* Run the Application! */
-    app({
-        window: window,
-        frameFactory: frameFactory,
-        $: c6Query,
-        Q: q,
-        config: config,
-        c6Ajax: c6Ajax,
-        documentParser: documentParser,
-        browserInfo: browserInfo,
-        Player: Player,
-        spCardService: spCards,
-        hostDocument: hostDocument,
-        Observable: Observable
-    });
-
-    return window.c6.loadExperience;
-}
-
-function fetchExperience(config) {
-    'use strict';
-
-    return new q.Promise(function(resolve) {
-        var base = parseUrl(config.apiRoot);
-
-        importScripts([formatUrl({
-            protocol: base.protocol,
-            hostname: base.hostname,
-            pathname: '/api/public/content/experience/' + config.id + '.js',
-            query: omit(config, ['id', 'apiRoot'])
-        })], function(experience) {
-            resolve(experience);
-        });
-    });
-}
-
 function initLogger(config) {
-    'use strict';
-
     var levels = [
         { value: 0, levels: ['error'] },
         { value: 1, levels: ['info', 'warn'] },
@@ -221,74 +49,51 @@ function initLogger(config) {
     }, []);
 
 
-    globalLogger.meta.container = config.src;
+    globalLogger.meta.container = config.container;
     globalLogger.meta.network = config.network;
-    globalLogger.meta.app = config.app;
+    globalLogger.meta.app = config.hostApp;
     globalLogger.levels(levels);
 }
 
-module.exports = function c6mraid(config) {
-    'use strict';
+module.exports = function c6mraid(/*config*/) {
+    var config = extend({
+        type: 'full-np',
+        apiRoot: 'https://portal.cinema6.com',
+        pageUrl: 'cinema6.com',
+        forceOrientation: 'portrait'
+    }, arguments[0]);
 
     initLogger(config);
 
-    var START_TIME = Date.now();
-    var apiRoot = config.apiRoot || 'https://portal.cinema6.com';
-    var pageUrl = config.pageUrl || 'cinema6.com';
-    var orientation = config.forceOrientation || 'portrait';
-    var loadExperience = getLoader(apiRoot);
-    var mraid = new MRAID({ forceOrientation: orientation, useCustomClose: true });
-    var ga = googleAnalytics('__c6_ga__', config.exp.replace(/^e-/, ''), window.c6.gaAcctIdEmbed, {
-        storage: 'none',
-        cookieDomain: 'none'
+    var endpoint = resolveUrl(config.apiRoot, '/api/public/players/' + config.type);
+    var player = new Player(endpoint, extend(pick(config, [
+        'experience', 'campaign', 'container', 'hostApp', 'network',
+        'branding', 'placementId', 'wildCardPlacement', 'pageUrl',
+        'preview'
+    ]), { standalone: false, interstitial: true, context: 'mraid' }));
+    var mraid = new MRAID({ forceOrientation: config.forceOrientation, useCustomClose: true });
+
+    player.bootstrap(document.body, {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    }).then(function logPlayerLoaded() {
+        logger.info('Player loaded.');
     });
-    var controller = null;
 
     logger.info('Initialize.', config);
-
-    ga('require', 'displayfeatures');
-    ga('set', {
-        checkProtocolTask: null,
-        dimension1: 'mraid',
-        page: pagePathFor(config.exp, {
-            cx: 'mraid',
-            ct: config.src,
-            ex: config.ex,
-            vr: config.vr
-        })
-    });
-    ga('send', 'pageview', {
-        sessionControl: 'start'
-    });
 
     mraid.on('error', function logError(message, action) {
         logger.error('MRAID Error:', message, action);
     });
 
     mraid.waitUntilViewable().then(function sendVisibleEvent() {
-        var visibleStart = Date.now();
-
-        ga('send', 'event', {
-            eventCategory: 'Display',
-            eventAction: 'Visible'
-        });
-
         logger.info('MRAID is reporting ad is viewable. useCustomClose is', mraid.useCustomClose);
 
         mraid.on('stateChange', function(state) {
-            var timeVisible = Date.now() - visibleStart;
-            var wasActive = !!controller;
-
             logger.info('MRAID reports state changed to', state);
-
-            if (state !== 'hidden')  { return; }
-
-            ga('send', 'timing', {
-                timingCategory: 'API',
-                timingVar: wasActive ? 'closePageAfterLoad' : 'closePageBeforeLoad',
-                timingValue: timeVisible,
-                timingLabel: 'c6'
-            });
         });
     });
 
@@ -301,93 +106,18 @@ module.exports = function c6mraid(config) {
         logger.info('Player event: ' + data.event, data);
     });
 
-    return q.all([
-        fetchExperience({
-            id: config.exp,
-            apiRoot: apiRoot,
-
-            branding: config.branding,
-            placementId: config.adPlacementId,
-            campaign: config.campaign,
-            container: config.src,
-            wildCardPlacement: config.wp,
-            preview: config.preview,
-            pageUrl: pageUrl,
-            hostApp: config.app,
-            network: config.network
-        }).then(function(experience) {
-            logger.info('Experience was fetched.');
-
-            ga('set', { title: experience.data.title });
-            ga('send', 'timing', {
-                timingCategory: 'API',
-                timingVar: 'fetchExperience',
-                timingValue: Date.now() - START_TIME,
-                timingLabel: 'c6'
-            });
-
-            experience.data.deck[0].data.preload = false;
-
-            return loadExperience({
-                load: true,
-                preload: true,
-
-                standalone: false,
-                interstitial: true,
-                playerVersion: config.playerVersion || 1,
-                mobileMode: config.mobileMode,
-                mode: config.mode,
-
-                embed: document.body,
-                splashDelegate: {},
-                experience: experience,
-
-                config: {
-                    exp: experience.id,
-                    title: experience.data.title,
-                    startPixel: (config.startPixels || []).join(' '),
-                    countPixel: (config.countPixels || []).join(' '),
-                    launchPixel: (config.launchPixels || []).join(' '),
-                    container: config.src,
-                    context: 'mraid',
-                    preview: config.preview,
-                    ex: config.ex,
-                    vr: config.vr,
-                    hostApp: config.app,
-                    network: config.network,
-                    pageUrl: pageUrl
-                }
-            }, true);
-        }).then(function logSuccess(controller) {
-            logger.info('Player loaded.');
-            return controller;
-        }),
-        mraid.waitUntilViewable().delay(600).then(function recordReadyTime() {
-            return Date.now();
-        })
-    ]).then(function activatePlayer(data) {
-        var waitTime = Date.now() - data[1];
-        controller = data[0];
-
+    return mraid.waitUntilViewable().delay(600).then(function activatePlayer() {
         logger.info('Showing player.');
 
-        ga('send', 'timing', {
-            timingCategory: 'API',
-            timingVar: 'loadDelay',
-            timingValue: waitTime,
-            timingLabel: 'c6'
-        });
+        player.show();
 
-        controller.state.set('active', true);
-        controller.state.observe('active', function observeActive(active) {
-            if (!active) {
-                mraid.close();
-            }
+        player.session.on('close', function closeMRAID() {
+            mraid.close();
         });
         mraid.on('stateChange', function handleAdStateChange(state) {
             if (state === 'hidden') {
                 logger.info('Hiding player.');
-                controller.state.set('active', false);
+                player.hide();
             }
         });
     });
